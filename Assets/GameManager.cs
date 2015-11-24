cb3903e985dda35d861b5cf7dbf0722968b9af45
Felipe Vargas <fvargasx@gmail.com>
2015-11-23 23:07:16 -0500

    Disable colliders on characters not in scene

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -80,8 +80,14 @@ public class GameManager : MonoBehaviour {
 			Renderer r = ((GameObject)de.Key).GetComponentsInChildren<Renderer>()[0];
 			if ((int)de.Value == player_state) {
 				r.enabled = true;
+				foreach (Collider c in ((GameObject)de.Key).GetComponentsInChildren<Collider>()) {
+					c.enabled = true;
+				}
 			} else {
 				r.enabled = false;
+				foreach (Collider c in ((GameObject)de.Key).GetComponentsInChildren<Collider>()) {
+					c.enabled = false;
+				}
 			}
 		}
 	}
